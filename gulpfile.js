7e1a15dfdc566825bfe585b8bc2f087e92ff406a
Denis <denis.dmitriyeff@gmail.com>
2019-03-14 00:11:45 +0200

    gulp settings

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require("gulp");
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+gulp.task("default", ["styles"], function() {
+  gulp.watch("sass/**/*.scss", ["styles"]);
+  browserSync.init({
+    server: "./",
+    files: ["sass/**/*.scss"] // specify files what browserSync needs to trigger when changed
+  });
+  console.log('hello there!');
+  // code for your default task goes here
+});
+
+gulp.task("styles", function() {
+  gulp
+    .src("sass/**/*.scss")
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(
+      autoprefixer({
+      browsers: ['last 2 versions']
+      })
+    )
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
+});
