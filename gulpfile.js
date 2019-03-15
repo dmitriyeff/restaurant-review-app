a8fd269e611139f2b5610e8258147140cd03a0c6
Denis <denis.dmitriyeff@gmail.com>
2019-03-15 17:31:49 +0200

    html, js browserSync, responsiveness of the index.html update

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,13 @@ const browserSync = require('browser-sync').create();
 
 gulp.task("default", ["styles"], function() {
   gulp.watch("sass/**/*.scss", ["styles"]);
+  gulp.watch("*.html").on('change', browserSync.reload); // browser reloads on HTML changes
+  gulp.watch("js/**/*.js").on('change', browserSync.reload); // browser reloads on JS files change
   browserSync.init({
     server: "./",
     files: ["sass/**/*.scss"] // specify files what browserSync needs to trigger when changed
   });
+
   console.log('hello there!');
   // code for your default task goes here
 });
